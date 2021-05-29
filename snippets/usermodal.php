c24eed2c1843a2245bcff88ed08e352b6b71550a
webdevsk <72684302+webdevsk@users.noreply.github.com>
2021-05-29 12:26:36 +0600

    Fetch Api

--- snippets/usermodal.php
+++ snippets/usermodal.php
@@ -14,9 +14,7 @@
             </div>
             <div class="col-md-8 d-flex">
               <div class="card-body align-self-center">
-                <h5 class="card-title"><?php echo $_SESSION['email']; //kjijijijij?></h5>
-                <!-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+                <h5 class="card-title"><?php echo $_SESSION['email']?></h5>
               </div>
             </div>
           </div>
--- update.php
+++ update.php
@@ -1,11 +1,6 @@
 
 <?php
-//Enter page title here
-$page_title = 'Updated Data';
 
-//importing header
-require_once 'snippets/header.php';
-//importing database connection
 require_once 'db/conn.php';
 
 
@@ -23,23 +18,14 @@ if(isset($_POST['submit'])){
     //call function to update and check if success or not
     $result = $crud->update($id, $fname, $lname, $email, $phone_number, $dob, $feedback, $subject_id);
 
-
-
     if($result){
-        header("Location: datalist.php?updated=1");
+        header('Location: datalist.php?updated=1');
         exit();
-    }else{ ?>
-        <div class="msgboard col-11 py-5 rounded-3 border border-light mx-auto mt-5 text-center bg-dark">
-            <h1 class='text-grad'><?php echo "error" ?></h1>
-        </div>
-    <?php } 
+    }else{ 
+        echo "error";
+    } 
  
     
-}else{ ?>
-    <div class="msgboard col-11 py-5 rounded-3 border border-light mx-auto mt-5 text-center bg-dark">
-        <h1 class='text-grad'><?php echo "error" ?></h1>
-    </div>
-<?php }
-
-
-require 'snippets/footer.php';
+}else{ 
+    header('Location: index.php');
+}
