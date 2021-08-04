e8f62f4b6ec7c684417050a03c9a0dca58b8c16d
webdevsk <72684302+webdevsk@users.noreply.github.com>
2021-08-04 12:06:30 +0600

    Update conn.php

--- db/conn.php
+++ db/conn.php
@@ -1,16 +1,16 @@
 <?php
     //Connection to the database
 
-    $host = 'remotemysql.com';
-    $db = '1Vlo3X8QkC';
-    $user = '1Vlo3X8QkC';
-    $pass = 'HiUkQaHIZd';
-    $charset = 'utf8mb4';
-    // $host = 'localhost';
-    // $db = 'ssb_db';
-    // $user = 'root';
-    // $pass = '';
+    // $host = 'remotemysql.com';
+    // $db = '1Vlo3X8QkC';
+    // $user = '1Vlo3X8QkC';
+    // $pass = 'HiUkQaHIZd';
     // $charset = 'utf8mb4';
+    $host = 'localhost';
+    $db = 'ssb_db';
+    $user = 'root';
+    $pass = '';
+    $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
@@ -18,8 +18,9 @@
         $pdo = new PDO($dsn, $user, $pass);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $error){
-        throw new PDOException($error->getMessage());
-        //echo "<h1 class='text-danger text-center'>Database error</h1>";
+        // throw new PDOException($error->getMessage());
+        echo $error->getMessage();
+        echo "<h1 class='text-danger text-center'>Database error</h1>";
     }
 
     require_once 'crud.php';
